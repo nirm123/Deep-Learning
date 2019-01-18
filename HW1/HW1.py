b4856947b410a20a43210d48e79125e3632ed204
Nirmal <nirmalp@illinois.edu>
2019-01-18 05:37:36 -0600

    starting homework 1

---
+++ HW1/HW1.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+import h5py
+import time
+import copy
+from random import randint
+
+# Load MNIST data
+MNIST = h5py.File('MNISTdata.hdf5', 'r')
+x_train = np.float32(MNIST['x_train'][:])
+y_train = np.int32(np.array(MNIST['y_train'][:,0]))
+
+print(x_train)
+
+x_test = np.float32(MNIST['x_test'][:])
+y_test = np.int32(np.array(MNIST['y_test'][:,0]))
+
+MNIST.close()
+
