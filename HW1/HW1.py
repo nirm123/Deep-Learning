2a2be2f900ca84e627c67cad248f347fa6ba127b
Nirmal <nirmalp@illinois.edu>
2019-01-23 03:54:12 -0600

    Almost done

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -13,29 +13,31 @@ def loss(theta, x, y):
     loss = 0
     for i in range(len(x)):
         soft = softmax(np.matmul(theta, x[i]))
-        loss -= np.log(soft[y])
-        
-    return loss/len(x)
+        loss -= np.log(soft[y[i]])
+    return loss
 
 def gradient(x, y, theta):
     dtheta = np.copy(theta)
-    elem = np.random.randint(0, len(x))
-    x_cur = x[elem]
-    y_cur = y[elem]
-    for z in range(len(theta)):
-        soft = softmax(np.matmul(theta, x_cur))
-        softZ = soft[z]
-        if z == y_cur:
-            dtheta[z] = -1*(1 - softZ)*x_cur
+    for i in range(5):
+        elem = np.random.randint(0, len(x))
+        x_cur = x[elem]
+        y_cur = y[elem]
+        for z in range(len(theta)):
+            soft = softmax(np.matmul(theta, x_cur))
+            softZ = soft[z]
+            if z == y_cur:
+                dtheta[z] = -1*(1 - softZ)*x_cur
+            else:
+                dtheta[z] = -1*(-softZ)*x_cur
+        if i == 0:
+            dtsum = dtheta
         else:
-            dtheta[z] = -1*(-softZ)*x_cur
-
-    return dtheta
+            dtsum += dtheta
+    return  dtsum/5
 
 def predict(weight, img):
     soft_res = softmax(np.matmul(weight, img))
     return np.argmax(soft_res)
-    
 
 if __name__ == "__main__":
     # Load MNIST data
@@ -47,19 +49,30 @@ if __name__ == "__main__":
 
     MNIST.close()
 
-    # Initialize weights
+    # Initialize variables
     weight = np.random.rand(10,784)
     alpha = 0.01
     iteration = 0
+    L_prev = loss(weight, x_train, y_train)
+    first = False
+    print(str(L_prev) + " 0")
+
     # Training
     while True:
         grad = gradient(x_train, y_train, weight)
         weight = np.subtract(weight, alpha*grad)
-        #print(loss(weight, x_train, y_train))
         iteration += 1
-        if iteration == 1000:
-            break
+        if iteration % 50 == 0:
+            L = loss(weight, x_train, y_train)
+            print(str(L) + " " + str(iteration))
+            if L < 26000 and first == False:
+                alpha /= 10
+                first = True
+            if L < 24000:
+                break
 
+    print("\nNumber of Iterations: " + str(iteration))
+    print("Final Alpha: " + str(alpha) + "\n")
     # Testing
     total_correct = 0
     for n in range(len(x_test)):
