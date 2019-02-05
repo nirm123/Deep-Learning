36d1f576607a6369e9397b294c391caaf108a667
Nirmal <nirmalp@illinois.edu>
2019-02-05 16:31:46 -0600

    Done?

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -52,6 +52,15 @@ def forward(weight, img):
     return_object = [soft_res, Z, H, U]
     return return_object
 
+def test(x_given, y_given):
+    total_correct = 0
+    for n in range(len(x_given)):
+        y = y_given[n]
+        x = x_given[n][:]
+        p = np.argmax(forward(theta,x)[0])
+        if p == y:
+            total_correct += 1
+    print(total_correct/np.float(len(x_test)))
 
 if __name__ == "__main__":
     # Load MNIST data
@@ -64,91 +73,26 @@ if __name__ == "__main__":
     MNIST.close()
 
     # Initialize variables
-    weight = [np.random.randn(50,784)/np.sqrt(784), np.random.randn(10,50)/np.sqrt(784)]
-    bias = [np.random.randn(50)/np.sqrt(784), np.random.randn(10)/np.sqrt(784)]
+    weight = [np.random.randn(64,784)/np.sqrt(784), np.random.randn(10,64)/np.sqrt(784)]
+    bias = [np.random.randn(64)/np.sqrt(784), np.random.randn(10)/np.sqrt(784)]
     theta = [weight, bias]
     
     alpha = 0.01
     iteration = 0
-    epoch = 0 
 
     # Training
     while True:
         grad = gradient(x_train, y_train, theta)
+    
         for i in range(4):
             theta[int(i/2)][i%2] = theta[int(i/2)][i%2] - alpha * grad[i]
+        
         iteration += 1
-        print(iteration)
-        if iteration == 1000:
-            iteration = 0
-            epoch += 1
-            break
-    '''
-    # Training
-    while True:
-        grad = gradient(x_train, y_train, theta)
-        dp_dU = gradient(x_select, y_select, forward_output)
         
-        dp_db2 = np.sum(dp_dU)/5
-        
-        dpdC = np.empty([5,10,50])
-        sigma = np.empty([5,50,1])
-        sigma_prime = np.empty([5,50])
-        dpdb1 = np.empty([5,50,1])
-        dpdW = np.empty([5,50,784])
-        for i in range(5):
-            dpdC[i] = np.matmul(dp_dU[i][np.newaxis].T,forward_output[i][2][np.newaxis])
-            sigma[i] = np.matmul(theta[0][1].T, dp_dU[i][:,None])
-            for j in range(50):
-                if forward_output[i][2][j] > 0:
-                    sigma_prime[i][j] = 1
-                else:
-                    sigma_prime[i][j] = 0
-            dpdb1[i] = np.multiply(sigma[i],sigma_prime[i][:,None])
-            dpdW[i] = np.multiply(dpdb1[i],theta[0][0])
-        
-        dp_dC = np.sum(dpdC)/5
-        dp_db1 = np.sum(dpdb1)/5
-        dp_dW = np.sum(dpdW)/5
-
-        theta[0][0] = theta[0][0] - alpha*dp_dW
-        theta[0][1] = theta[0][1] - alpha*dp_dC
-        theta[1][0] = theta[1][0] - alpha*dp_db1
-        theta[1][1] = theta[1][1] - alpha*dp_db2
-
-        iteration += 5
         if iteration == 60000:
-            print('Progress')
-            iteration = 0
-            epoch += 1
-            if epoch == 5:
-                break
-    '''
-    '''
-    L_prev = loss(weight, x_train, y_train)
-    print(str(L_prev) + " 0")
-
-    # Training
-    while True:
-        grad = gradient(x_train, y_train, weight)
-        weight = np.subtract(weight, alpha*grad)
-        iteration += 1
-        if iteration % 50 == 0:
-            L = loss(weight, x_train, y_train)
-            print(str(L) + " " + str(iteration))
-        if iteration == 20000:
+            alpha = alpha/10
+        if iteration == 120000:
             break
-
-    print("\nNumber of Iterations: " + str(iteration))
-    print("Final Alpha: " + str(alpha) + "\n")
-    '''
-
+    
     # Testing
-    total_correct = 0
-    for n in range(len(x_test)):
-        y = y_test[n]
-        x = x_test[n][:]
-        p = np.argmax(forward(theta,x)[0])
-        if p == y:
-            total_correct += 1
-    print(total_correct/np.float(len(x_test)))
+    test(x_test, y_test)
