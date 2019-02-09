7171bea1fb6321c4f5837b5274c36063f332ad7f
Nirmal <nirmalp@illinois.edu>
2019-02-09 10:00:34 -0600

    no idea

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -6,79 +6,91 @@ import copy
 from random import randint
 
 # Function that calculates the softmax
-def softmax(input_vector):
+def softmax(input_v):
+    input_vector = input_v.copy()
+    print(input_vector)
+    print()
     exp_vec = np.exp(input_vector)
     return np.divide(exp_vec, np.sum(exp_vec))
 
 # Function that calculates loss
 def loss(theta, x, y):
     loss = 0
+    print("\nCalculating Loss:")
     for i in range(len(x)):
         soft = forward(theta, x[i])[0]
         loss -= np.log(soft[y[i]])
     return loss
 
 # Function that applies reLU to a vector
-def reLU(vector):
+def reLU(vec):
+    vector = vec.copy()
     vector[vector < 0] = 0
     return vector
 
 # Function that calculates the derivative of the reLU function
-def reLU_prime(vector):
+def reLU_prime(vec):
+    vector = vec.copy()
     vector[vector > 0] = 1
     return vector
 
 # Function that calcuate the gradient for all parameters
 def gradient(x, y, theta):
     elem = np.random.randint(0, len(x))
+    #elem = 30807
+    print(elem)
     x_cur = x[elem]
     y_cur = y[elem]
     
     output = forward(theta, x_cur)
 
-    dp_dU = np.empty(10)
+    dp_dU = np.zeros(10)
     for z in range(len(dp_dU)):
             softZ = output[0][z]
             if z == y_cur:
                 dp_dU[z] = -1*(1 - softZ)
             else:
                 dp_dU[z] = -1*(-softZ)
-    dp_db = dp_dU
+    db_dU = dp_dU
 
-    '''
-    dp_dC = np.matmul(dp_dU[:,None], output[2][:,None].T)
-    sigma = np.matmul(theta[0][1].T, dp_dU)
-    dp_db1 = np.multiply(sigma, reLU_prime(output[2])) 
-    dp_dW = np.matmul(dp_db1[:,None], x_cur[:,None].T)
-    return [dp_dW, dp_dC, dp_db1, dp_db2]
-    '''
-    return [dp_db]
+    sigma = np.zeros([3,25,25])
+    dK_dU = np.zeros([3,4,4])
+    dW_dU = np.zeros([3,10,25,25])
+
+    for i in range(3):
+        for j in range(10):
+            sigma[i] += dp_dU[j] * theta[1][i][j]
+            dW_dU[i][j] = dp_dU[j] * output[2][i]
+    for channel in range(3):
+        dK_dU[channel] = convolution((reLU_prime(output[3][channel])*sigma[channel]),x_cur.reshape(28,28))
+    return [dK_dU,dW_dU,db_dU]
 
 # Function that calculates forward pass of neural network
 def forward(weight, img):
-    Z = np.empty([3,24,24])
-    H = np.empty([3,24,24])
-    tempI = 0
-    tempJ = 0
-    tempP = 0
-    for p in range(3):
-        for i in range(24):
-            for j in range(24):
-                Z[p][i][j] = np.sum(weight[0][p]*(img.reshape(28,28)[j:j+4,i:i+4]))
-    
-    H = np.copy(Z)
-    reLU(H)
-    
-    U = np.empty(10)
-    for q in range(10):
-        for w in range(3):
-            for i in range(24):
-                for j in range(24):
-                    U[q] += weight[1][q][p][i][j]*H[p][i][j]
-    U += weight[2]
+    Z = np.zeros([3,25,25])
+    H = np.zeros([3,25,25])
+    U = np.zeros([10])
+    for channel in range(3):
+        Z[channel] = convolution(weight[0][channel],img.reshape(28,28))
+    H = reLU(Z)
+    for channel in range(3):
+        for k in range(10):
+            for i in range(25):
+                for j in range(25):
+                    U[k] += weight[1][channel][k][i][j] * H[channel][i][j]
     soft_res = softmax(U)
-    return_object = [soft_res, U, H, Z]
-    return return_object
+
+    return [soft_res, U, H, Z]
+
+def convolution(array1, array2):
+    kernel_size = len(array1)
+    dim = len(array2) - kernel_size + 1
+    result = np.empty([dim,dim])
+    for i in range(dim):
+        for j in range(dim):
+            result[i][j] = np.sum(array1*array2[i:i+kernel_size,j:j+kernel_size])
+    return result
+
 
 # Function that calculates accuracy of prediction
 def test(x_given, y_given):
@@ -106,31 +118,28 @@ if __name__ == "__main__":
 
     # Initialize variables
     K = np.random.uniform(-1,1,(3,4,4))
-    W = np.random.uniform(-1,1,(10,3,24,24))
+    W = np.random.uniform(-1,1,(3,10,25,25))
     b = np.random.uniform(-1,1,(10))
     theta = [K, W, b]
 
     alpha = 0.01
     iteration = 0
     epoch = 0
-   
-    # Training
+    
     while True:
-
-        # Calculate gradient for random element 
         grad = gradient(x_train, y_train, theta)
-        theta[2] -= alpha * grad[0]    
-        '''
-        # Take step for all parameters based on alpha and gradient
-        for i in range(4):
-            theta[int(i/2)][i%2] = theta[int(i/2)][i%2] - alpha * grad[i]
-        '''
+        for channel in range(3):
+            theta[0][channel] -= alpha * grad[0][channel]
+            theta[1][channel] -= alpha * grad[1][channel]
+        theta[2] -= alpha * grad[2]
 
+        print(theta[0])
+        print(theta[1])
+        print(theta[2])
         iteration += 1
-        # Increment epoch every 20000 iterations
+        if iteration % 100 == 0:
+            print(iteration)
         if iteration == 1000:
-            epoch += 1
-            print(epoch)
             break
     # Testing
-    test(x_test, y_test)
+    #test(x_test, y_test)
