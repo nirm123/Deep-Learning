3711f8e69c0d18ecae3f56877c4d9499ebd182ae
Nirmal <nirmalp2@illinois.edu>
2019-02-27 08:22:42 -0600

    changes

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -27,36 +27,36 @@ class cifarModel(nn.Module):
     def __init__(self): 
         super(cifarModel, self).__init__()
         # (w - k + 2p)/s + 1
-        self.conv1 = nn.Conv2d(3, 64, 4, 1, 2) # (32 - 4 + 4)/1 + 1 = 33
-        self.norm1 = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(64, 64, 4, 1, 2) # (32 - 4 + 4)/1 + 1 = 34
+        self.conv1 = nn.Conv2d(3, 32, 4, 1, 2) # (32 - 4 + 4)/1 + 1 = 33
+        self.norm1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 4, 1, 2) # (32 - 4 + 4)/1 + 1 = 34
         
         self.pool = nn.MaxPool2d(2, 2) # (32 - 0 + 0)/2 = 17
         self.drop1 = nn.Dropout(0.5)
         
-        self.conv3 = nn.Conv2d(64, 64, 4, 1, 2) # (17 - 4 + 4)/1 + 1 = 18
-        self.norm2 = nn.BatchNorm2d(64)
-        self.conv4 = nn.Conv2d(64, 64, 5, 1, 2) # (18 - 5 + 4)/1 + 1 = 18
+        self.conv3 = nn.Conv2d(64, 128, 4, 1, 2) # (17 - 4 + 4)/1 + 1 = 18
+        self.norm2 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 5, 1, 2) # (18 - 5 + 4)/1 + 1 = 18
         
         # (self.pool1) (18 - 0 + 0)/2 = 9
         self.drop2 = nn.Dropout(0.5)
 
-        self.conv5 = nn.Conv2d(64, 64, 4, 1, 2) # (9 - 4 + 4)/1 + 1 = 10
-        self.norm3 = nn.BatchNorm2d(64)
-        self.conv6 = nn.Conv2d(64, 64, 3, 1) # (10 - 3 + 0)/1 + 1 = 8
+        self.conv5 = nn.Conv2d(256, 256, 4, 1, 2) # (9 - 4 + 4)/1 + 1 = 10
+        self.norm3 = nn.BatchNorm2d(256)
+        self.conv6 = nn.Conv2d(256, 256, 3, 1) # (10 - 3 + 0)/1 + 1 = 8
 
         self.drop3 = nn.Dropout(0.5)
 
-        self.conv7 = nn.Conv2d(64, 64, 3, 1) # (8 - 3 + 0)/1 + 1 = 6
-        self.norm4 = nn.BatchNorm2d(64)
-        self.conv8 = nn.Conv2d(64, 64, 3, 1) # (8 - 3 + 0)/1 + 1 = 4
+        self.conv7 = nn.Conv2d(256, 256, 3, 1) # (8 - 3 + 0)/1 + 1 = 6
+        self.norm4 = nn.BatchNorm2d(256)
+        self.conv8 = nn.Conv2d(256, 256, 3, 1) # (8 - 3 + 0)/1 + 1 = 4
 
-        self.norm5 = nn.BatchNorm2d(64)
+        self.norm5 = nn.BatchNorm2d(256)
         self.drop4 = nn.Dropout(0.5)
 
-        self.full1 = nn.Linear(64 * 4 * 4, 500)
-        self.full2 = nn.Linear(500, 250)
-        self.full3 = nn.Linear(250, 10)
+        self.full1 = nn.Linear(256 * 4 * 4, 1000)
+        self.full2 = nn.Linear(1000, 500)
+        self.full3 = nn.Linear(500, 10)
 
     def forward(self, x):
         x = F.relu(self.conv2(self.norm1(F.relu(self.conv1(x)))))
@@ -71,7 +71,7 @@ class cifarModel(nn.Module):
 
         x = self.drop4(self.norm5(x))
 
-        x = x.view(-1, 64 * 4 * 4)
+        x = x.view(-1, 256 * 4 * 4)
         x = F.relu(self.full1(x))
         x = F.relu(self.full2(x))
         x = self.full3(x)
@@ -105,9 +105,10 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuff
 testset = torchvision.datasets.CIFAR10(root='./', train=False, download=False, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
 
-use_gpu = torch.cuda.is_available()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model = cifarModel()
+model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -124,9 +125,8 @@ for epoch in range(num_epochs):
         if(Y_train_batch.shape[0]<batch_size):
             continue
         
-        #if use_gpu:
-        #    X_train_batch = Variable(X_train_batch).cuda()
-        #    Y_train_batch = Variable(Y_train_batch).cuda()
+        X_train_batch = X_train_batch.to(device)
+        Y_train_batch = Y_train_batch.to(device)
         
         output = model(X_train_batch)
         curr_loss = criterion(output, Y_train_batch)
@@ -148,6 +148,8 @@ with torch.no_grad():
     correct = 0
     total = 0
     for X_test_batch, Y_test_batch in trainloader:
+        X_test_batch = X_test_batch.to(device)
+        Y_test_batch = Y_test_batch.to(device)
         output = model(X_test_batch)
         _, predicted = torch.max(output.data, 1)
         total += Y_test_batch.size(0)
