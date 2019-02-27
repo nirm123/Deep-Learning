ef2a1222c582dda88a61a0fdda8bdc75e1c9e6ba
Nirmal <nirmalp2@illinois.edu>
2019-02-27 07:00:53 -0600

    basically working

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -24,49 +24,57 @@ no_of_hidden_units = 196
 
 # Define Model
 class cifarModel(nn.Module):
-    def __init__(self):
+    def __init__(self): 
         super(cifarModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 4, 1, 2)
+        # (w - k + 2p)/s + 1
+        self.conv1 = nn.Conv2d(3, 64, 4, 1, 2) # (32 - 4 + 4)/1 + 1 = 33
         self.norm1 = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(64, 64, 4, 1, 2)
+        self.conv2 = nn.Conv2d(64, 64, 4, 1, 2) # (32 - 4 + 4)/1 + 1 = 34
         
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.drop1 = nn.Dropout(0.2)
+        self.pool = nn.MaxPool2d(2, 2) # (32 - 0 + 0)/2 = 17
+        self.drop1 = nn.Dropout(0.5)
         
-        self.conv3 = nn.Conv2d(64, 64, 4, 1, 2)
+        self.conv3 = nn.Conv2d(64, 64, 4, 1, 2) # (17 - 4 + 4)/1 + 1 = 18
         self.norm2 = nn.BatchNorm2d(64)
-        self.conv4 = nn.Conv2d(64, 64, 5, 1, 2)
+        self.conv4 = nn.Conv2d(64, 64, 5, 1, 2) # (18 - 5 + 4)/1 + 1 = 18
         
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.drop2 = nn.Dropout(0.2)
+        # (self.pool1) (18 - 0 + 0)/2 = 9
+        self.drop2 = nn.Dropout(0.5)
 
-        self.conv5 = nn.Conv2d(64, 64, 4, 1, 2)
+        self.conv5 = nn.Conv2d(64, 64, 4, 1, 2) # (9 - 4 + 4)/1 + 1 = 10
         self.norm3 = nn.BatchNorm2d(64)
-        self.conv6 = nn.Conv2d(64, 64, 3, 1)
+        self.conv6 = nn.Conv2d(64, 64, 3, 1) # (10 - 3 + 0)/1 + 1 = 8
 
-        self.drop3 = nn.Dropout(0.2)
+        self.drop3 = nn.Dropout(0.5)
 
-        self.conv7 = nn.Conv2d(64, 64, 3, 1)
+        self.conv7 = nn.Conv2d(64, 64, 3, 1) # (8 - 3 + 0)/1 + 1 = 6
         self.norm4 = nn.BatchNorm2d(64)
-        self.conv8 = nn.Conv2d(64, 64, 3, 1)
+        self.conv8 = nn.Conv2d(64, 64, 3, 1) # (8 - 3 + 0)/1 + 1 = 4
 
         self.norm5 = nn.BatchNorm2d(64)
-        self.drop4 = nn.Dropout(0.2)
+        self.drop4 = nn.Dropout(0.5)
 
-        self.full1 = nn.Linear(4, 500)
+        self.full1 = nn.Linear(64 * 4 * 4, 500)
         self.full2 = nn.Linear(500, 250)
         self.full3 = nn.Linear(250, 10)
 
     def forward(self, x):
         x = F.relu(self.conv2(self.norm1(F.relu(self.conv1(x)))))
-        x = self.drop1(self.pool1(x))
+        x = self.drop1(self.pool(x))
+
         x = F.relu(self.conv4(self.norm2(F.relu(self.conv3(x)))))
-        x = self.drop2(self.pool2(x))
-        x = F.relu(self.conv6(self.norm3(F.relu(self.conv5(x)))))
-        x = self.drop3(x)
+        x = self.drop2(self.pool(x))
+
+        x = self.drop3(F.relu(self.conv6(self.norm3(F.relu(self.conv5(x))))))
+
         x = F.relu(self.conv8(self.norm4(F.relu(self.conv7(x)))))
+
         x = self.drop4(self.norm5(x))
-        x = self.full3(self.full2(self.full1(x)))
+
+        x = x.view(-1, 64 * 4 * 4)
+        x = F.relu(self.full1(x))
+        x = F.relu(self.full2(x))
+        x = self.full3(x)
         return x
 
 # Data Augmentation
@@ -108,15 +116,17 @@ start_time = time.time()
 loss = []
 accuracy = []
 
+model.train()
+
 # Train the model
 for epoch in range(num_epochs):
     for batch_idx, (X_train_batch, Y_train_batch) in enumerate(trainloader):
         if(Y_train_batch.shape[0]<batch_size):
             continue
         
-        if use_gpu:
-            X_train_batch = Variable(X_train_batch).cuda()
-            Y_train_batch = Variable(Y_train_batch).cuda()
+        #if use_gpu:
+        #    X_train_batch = Variable(X_train_batch).cuda()
+        #    Y_train_batch = Variable(Y_train_batch).cuda()
         
         output = model(X_train_batch)
         curr_loss = criterion(output, Y_train_batch)
@@ -127,11 +137,10 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         _, predicted = torch.max(output.data, 1)
-        correct = (predicted == labels).sum().item()
-        accuracy.append(correct/labels.size(0))
-
-        if i % 100 == 0:
-            print('Epoch: ' + str(epoch+1) + '/' + str(num_epochs) + ', Step: ' + str(i+1) + '/' + str(len(trainloader)) + ', Loss: ' + str(curr_loss.item()) + ', Accuracy: ' + str(correct/labels.size(0)*100) + '%')
+        correct = (predicted == Y_train_batch).sum().item()
+        accuracy.append(correct/Y_train_batch.size(0))
+        if batch_idx % 100 == 0:
+            print('Epoch: ' + str(epoch+1) + '/' + str(num_epochs) + ', Step: ' + str(batch_idx+1) + '/' + str(len(trainloader)) + ', Loss: ' + str(curr_loss.item()) + ', Accuracy: ' + str(correct/Y_train_batch.size(0)*100) + '%')
 
 # Test the model
 model.eval()
@@ -141,7 +150,7 @@ with torch.no_grad():
     for X_test_batch, Y_test_batch in trainloader:
         output = model(X_test_batch)
         _, predicted = torch.max(output.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        total += Y_test_batch.size(0)
+        correct += (predicted == Y_test_batch).sum().item()
 
     print('Test Accuracy: ' + str((correct/total) * 100) + '%')
