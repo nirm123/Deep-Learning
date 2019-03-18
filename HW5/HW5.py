fac2dcf0eca538c04d2d4616f4db0e933f162e46
Nirmal <nirmalp2@illinois.edu>
2019-03-18 13:39:24 -0500

    Revert "edits"
    This reverts commit 1a0f0e13ab5fa98cf6182a2875554ff56b3ffe48.

--- HW5/HW5.py
+++ HW5/HW5.py
@@ -16,7 +16,7 @@ import time
 import numpy as np
 
 # Hyper-parameters
-num_epochs = 40
+num_epochs = 100
 learning_rate = 0.001
 batch_size = 128
 DIM = 32
@@ -114,11 +114,11 @@ transform_test = transforms.Compose([
 ])
 
 # Load Training Data
-trainset = torchvision.datasets.CIFAR100(root='/projects/training/bawc/CIFAR100/Dataset', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.CIFAR100(root='./', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=8)
 
 # Load Testing Data
-testset = torchvision.datasets.CIFAR100(root='/projects/training/bawc/CIFAR100/Dataset', train=False, download=False, transform=transform_test)
+testset = torchvision.datasets.CIFAR100(root='./', train=False, download=False, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -137,6 +137,11 @@ model.train()
 
 # Train the model
 for epoch in range(num_epochs):
+    if epoch == 40:
+        learning_rate = learning_rate/10
+    if epoch == 60:
+        learning_rate = learning_rate/10
+        
     for batch_idx, (X_train_batch, Y_train_batch) in enumerate(trainloader):
         if(Y_train_batch.shape[0]<batch_size):
             continue
@@ -176,4 +181,19 @@ with torch.no_grad():
         total += Y_test_batch.size(0)
         correct += (predicted == Y_test_batch).sum().item()
 
-    print('Test Accuracy: ' + str((correct/total) * 100) + '%')
+    print('Test Accuracy (eval): ' + str((correct/total) * 100) + '%')
+
+# Test the model
+model.train()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for X_test_batch, Y_test_batch in trainloader:
+        X_test_batch = X_test_batch.to(device)
+        Y_test_batch = Y_test_batch.to(device)
+        output = model(X_test_batch)
+        _, predicted = torch.max(output.data, 1)
+        total += Y_test_batch.size(0)
+        correct += (predicted == Y_test_batch).sum().item()
+
+    print('Test Accuracy (train/no_grad): ' + str((correct/total) * 100) + '%')
